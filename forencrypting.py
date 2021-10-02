32f8738268b86dc559d4d958dd6e566c2458c396
Jason Wang <91506829+Leek727@users.noreply.github.com>
2021-10-02 03:20:17 +0000

    final version - injector working

---
+++ forencrypting.py
@@ -0,0 +1,19 @@
+# Random bullshit that we probably dont need 
+"""
+
+x = ""
+
+with open("onefile_coom.py","r") as f:
+	x = f.read()
+
+
+#with open("test.txt","w") as f:#
+	#encrypted = gpg_enc(str(input(":")), x)
+#f.write(encrypted)s
+encrypted = gpg_enc(str(input(":")), x)
+print(encrypted)
+#x = ""
+#with open("test.txt", "r") as f:
+#	x = f.read()
+
+"""
